60bbf06827e885a771d13d394faf53fcb1311b41
submonkey <hunt.if@gmail.com>
2013-07-30 10:49:49 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '9411701e353ea8644f06cd35b6c2bb01054160ddf8a74ab69955f162fcfd4d395f9492aac0db831e177fbb8eb579e7f9994b0d1870c6d9a52f87cca61d441d62'
